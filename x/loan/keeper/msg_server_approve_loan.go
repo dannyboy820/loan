6334bd18b326010e2166c82acbd5635b24354224
dannyboy820 <dannyboy.0193@gmail.com>
2024-05-17 00:38:30 +0900

    ignite scaffold message approve-loan id:uint

---
+++ x/loan/keeper/msg_server_approve_loan.go
@@ -0,0 +1,18 @@
+package keeper
+
+import (
+	"context"
+
+	"loan/x/loan/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) ApproveLoan(goCtx context.Context, msg *types.MsgApproveLoan) (*types.MsgApproveLoanResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgApproveLoanResponse{}, nil
+}
