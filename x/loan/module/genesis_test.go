b3f785831e01f182d494a51430c31ec9e2490d82
dannyboy820 <dannyboy.0193@gmail.com>
2024-05-17 00:31:57 +0900

    ignite scaffold list loan amount fee collateral deadline state borrower lender --no-message

--- x/loan/module/genesis_test.go
+++ x/loan/module/genesis_test.go
@@ -15,6 +15,15 @@ func TestGenesis(t *testing.T) {
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
 
+		LoanList: []types.Loan{
+			{
+				Id: 0,
+			},
+			{
+				Id: 1,
+			},
+		},
+		LoanCount: 2,
 		// this line is used by starport scaffolding # genesis/test/state
 	}
 
@@ -26,5 +35,7 @@ func TestGenesis(t *testing.T) {
 	nullify.Fill(&genesisState)
 	nullify.Fill(got)
 
+	require.ElementsMatch(t, genesisState.LoanList, got.LoanList)
+	require.Equal(t, genesisState.LoanCount, got.LoanCount)
 	// this line is used by starport scaffolding # genesis/test/assert
 }
--- x/loan/types/genesis.go
+++ x/loan/types/genesis.go
@@ -1,6 +1,8 @@
 package types
 
-// this line is used by starport scaffolding # genesis/types/import
+import (
+	"fmt"
+)
 
 // DefaultIndex is the default global index
 const DefaultIndex uint64 = 1
@@ -8,6 +10,7 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
+		LoanList: []Loan{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
 	}
@@ -16,6 +19,18 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	// Check for duplicated ID in loan
+	loanIdMap := make(map[uint64]bool)
+	loanCount := gs.GetLoanCount()
+	for _, elem := range gs.LoanList {
+		if _, ok := loanIdMap[elem.Id]; ok {
+			return fmt.Errorf("duplicated id for loan")
+		}
+		if elem.Id >= loanCount {
+			return fmt.Errorf("loan id should be lower or equal than the last id")
+		}
+		loanIdMap[elem.Id] = true
+	}
 	// this line is used by starport scaffolding # genesis/types/validate
 
 	return gs.Params.Validate()
