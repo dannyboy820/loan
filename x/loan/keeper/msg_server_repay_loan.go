8b983ad1c89575e26955d8b2c53fc8a1c603bf38
dannyboy820 <dannyboy.0193@gmail.com>
2024-05-17 00:40:14 +0900

    ignite scaffold message repay-loan id:uint

---
+++ x/loan/keeper/msg_server_repay_loan.go
@@ -0,0 +1,18 @@
+package keeper
+
+import (
+	"context"
+
+	"loan/x/loan/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) RepayLoan(goCtx context.Context, msg *types.MsgRepayLoan) (*types.MsgRepayLoanResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgRepayLoanResponse{}, nil
+}
