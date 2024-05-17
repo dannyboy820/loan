e6fdba983b8da1a05a5352d8b9882053e6d94ef9
dannyboy820 <dannyboy.0193@gmail.com>
2024-05-18 00:04:04 +0900

    added logic to request-loan message server

--- x/loan/keeper/msg_server_request_loan.go
+++ x/loan/keeper/msg_server_request_loan.go
@@ -11,8 +11,26 @@ import (
 func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan) (*types.MsgRequestLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
-
+	var loan = types.Loan{
+		Amount: msg.Amount,
+		Fee: msg.Fee,
+		Collateral: msg.Collateral,
+		Deadline: msg.Deadline,
+		Borrower: msg.Creator,
+		State: "requested",
+	}
+	borrower, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	collateral, err := sdk.ParseCoinsNormalized(loan.Collateral)
+	if err != nil {
+		panic(err)
+	}
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrower, types.ModuleName, collateral)
+	if sdkError != nil{
+		return nil, sdkError
+	}
+	k.AppendLoan(ctx, loan)
 	return &types.MsgRequestLoanResponse{}, nil
 }
